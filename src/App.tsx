5ae5c7a0b3d9692581aa81acadb4ce43be0cff94
minuxx <minux8905@gmail.com>
2023-03-07 03:27:17 +0900

    init project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './routes/Home'
+import Search from './routes/Search'
+import Tv from './routes/Tv'
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="movies/:id" element={<Home />} />
+        <Route path="/tv" element={<Tv />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
